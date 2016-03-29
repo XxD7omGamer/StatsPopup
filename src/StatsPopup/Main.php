7afa927e53b0b3faca5b2786b4354f1c85cfc53b
XxD7omGamer <d7omy2003@hotmail.com>
2016-03-29 15:56:31 +0300

    Add EconomyAPI

--- src/StatsPopup/Main.php
+++ src/StatsPopup/Main.php
@@ -11,6 +11,7 @@ class Main extends PluginBase implements Listener{
 	
 	public function onEnable(){
 			$this->getServer()->getPluginManager()->registerEvents($this, $this);
+			 $this->economyapi = $this->getServer()->getPluginManager()->getPlugin("EconomyAPI");
 	}
 	
 	public function onMove(\pocketmine\event\player\PlayerMoveEvent $event){
@@ -21,6 +22,6 @@ class Main extends PluginBase implements Listener{
 		$x = $player->getFloorX();
 		$y = $player->getFloorY();
 		$z = $player->getFloorZ();
-		$player->sendTip("\n\n•\n                                                 §3§l                                                   §e§lPlayers§a: §c".$pn."§2/§c".$px."§r\n                                                 §e§lHealth§a: §c".$player->getHealth()."§2/§c".$player->getMaxHealth());
+		$player->sendTip("\n\n•\n                                                 §3§l  §eSuper§cLG§r\n                                                 §e§lPlayers§a: §c".$pn."§2/§c".$px."§r\n                                                 §e§lCoins§a: §c".$this->economyapi->mymoney($name)."§r\n                                                 §e§lHealth§a: §c".$player->getHealth()."§2/§c".$player->getMaxHealth());
 	}
 }
