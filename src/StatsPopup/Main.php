6877a49633d1702dad8b75c188940ece5a306109
XxD7omGamer <d7omy2003@hotmail.com>
2016-03-29 15:54:21 +0300

    Create Main.php

---
+++ src/StatsPopup/Main.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace StatsPopup;
+
+use pocketmine\plugin\PluginBase;
+use pocketmine\event\Listener;
+
+class Main extends PluginBase implements Listener{
+	
+	private $economyapi;
+	
+	public function onEnable(){
+			$this->getServer()->getPluginManager()->registerEvents($this, $this);
+	}
+	
+	public function onMove(\pocketmine\event\player\PlayerMoveEvent $event){
+		$player = $event->getPlayer();
+		$pn = count($this->getServer()->getOnlinePlayers());
+		$px = $this->getServer()->getMaxPlayers();
+		$name = $player->getName();
+		$x = $player->getFloorX();
+		$y = $player->getFloorY();
+		$z = $player->getFloorZ();
+		$player->sendTip("\n\n•\n                                                 §3§l                                                   §e§lPlayers§a: §c".$pn."§2/§c".$px."§r\n                                                 §e§lHealth§a: §c".$player->getHealth()."§2/§c".$player->getMaxHealth());
+	}
+}
